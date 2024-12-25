cd135450bb875f3f4014edbbc902ed01829c2efc
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-12-25 14:32:13 +0530

    implement taking use input-Ongoing

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./ImageGenerator.css";
 import defaultImage from "../Assets/DefaultImage.jpeg";
 const ImageGenerator = () => {
+  const [imageUrl, setImageUrl] = useState("/");
+  let inputRef = useRef(null);
+const imageGenertor=()=>{
+    if(inputRef.current.value===''){
+        return 0;
+    }
+}
+
   return (
     <div className="ai-image-generator">
       <div className="header">AI Image Generator</div>
       <div className="image-loading">
         <div className="image">
-          <img src={defaultImage} alt="default image"></img>
+          <img
+            src={imageUrl === "/" ? defaultImage : imageUrl}
+            alt="default image"
+          ></img>
         </div>
       </div>
       <div className="search-box">
@@ -15,6 +26,7 @@ const ImageGenerator = () => {
           type="text"
           className="search-input"
           placeholder="write what you want"
+          ref={inputRef}
         />
         <div className="generate-btn">Generate images</div>
       </div>
